291e6b5906a51d8b6ac55329b7cad0ea4cbb6e1e
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2022-02-27 21:09:26 +0530

    added orders to mongodb

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,20 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Product = require("../models/product");
+const Order = require("../models/order");
+
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "hello from orders!",
-  });
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then((docs) => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map((doc) => {
+          return {
+            request: {
+              _id: doc._id,
+              product: doc.product,
+              quantity: doc.quantity,
+              type: "GET",
+              url: `http://localhost:3000/products/${doc.product}`,
+            },
+          };
+        }),
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.message,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity,
-  };
-  res.status(201).json({
-    message: "POST: hello from orders!",
-    order: order,
-  });
+  Product.findById(req.body.product)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({
+          message: "Product Not Found",
+        });
+      }
+      const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.product,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Order stored",
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity,
+        },
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/orders/" + result._id,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.get("/:orderId", (req, res, next) => {
@@ -29,4 +81,5 @@ router.get("/:orderId", (req, res, next) => {
     });
   }
 });
+
 module.exports = router;
